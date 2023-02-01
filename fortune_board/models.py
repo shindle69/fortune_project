e2992d3d0e299365ced45593398503c7cd675303
unknown <juyouso@gmail.com>
2023-02-01 15:34:11 +0900

    삭제된 작성자를 None으로 표시

--- fortune_board/models.py
+++ fortune_board/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from django.conf import settings
+# from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL, null=True)
     content = models.TextField()
     publish_confirm = models.BooleanField(default=False)
     
-    post_image = models.ImageField(upload_to='fortune_board/images/%Y/%m/%d/', blank=True)
-    file_upload = models.FileField(upload_to='fortune_board/files/%Y/%m/%d/', blank=True)
+    post_image = models.ImageField(upload_to='fortune_board/images/%Y/%m/%d/', blank=True, null=True)    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
-        return (self.title)
+        return self.title
 
     def get_absolute_url(self):
         return f'/fortune_board/{self.pk}/'
--- fortune_board/tests.py
+++ fortune_board/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        #포스트목록을 가져온다
+        response = self.client.get('/fortune_board/')  
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content,'html.parser')
+        self.assertEqual(soup.title.text,'궁물쌤')
+
+        navbar = soup.nav
+        self.assertIn('게시판', soup.text) #navbar.text error 생김
+        self.assertIn('About', soup.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        main_area = soup.find('div', id='main_area')
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
+
+        post_001 = Post.objects.create(
+            title = '첫 번째 홈페이지 포스트입니다.',
+            content = 'Hello World. Welcome to my world',
+        )
+        post_002 = Post.objects.create(
+            title = '두 번째 홈페이지 포스트입니다.',
+            content = 'Hello World2222222222. Welcome to my world',
+        )
+        self.assertEqual(Post.objects.count(), 2)
+
+        response = self.client.get('/fortune_board/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content,'html.parser')
+        main_area = soup.find('div', id='main_area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+
+
