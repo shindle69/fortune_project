2f30a6bda5cef37f31d41daa510a4d5d8958af22
unknown <juyouso@gmail.com>
2023-02-03 17:10:48 +0900

    포스트 수정할 때 텍스트로 태그 지정하기

--- fortune_board/views.py
+++ fortune_board/views.py
@@ -103,10 +103,19 @@ class PostCreate(LoginRequiredMixin,CreateView):
 class PostUpdate(LoginRequiredMixin,UpdateView):
     model = Post
     fields = [
-        'title', 'content', 'post_image', 'category','publish_confirm', 'tag'
+        'title', 'content', 'post_image', 'category','publish_confirm', 
     ]
 
     template_name = "fortune_board/post_update_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tag.exists():
+            tags_str_list = list()
+            for t in self.object.tag.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = ';'.join(tags_str_list)
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
@@ -114,7 +123,26 @@ class PostUpdate(LoginRequiredMixin,UpdateView):
         else:
             raise PermissionDenied
 
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tag.clear()
 
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+
+            tags_str = tags_str.replace(',',';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tagging, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tagging.slug = slugify(t, allow_unicode=True)
+                    tagging.save()
+                self.object.tag.add(tagging)
+
+        return response
 
 
 
